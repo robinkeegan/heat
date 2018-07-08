f5ff42e07b45c65f03b27e6f7b1c8cfb6cc764b2
Robin Keegan-Treloar <robin_kt@outlook.com>
2018-07-08 14:55:43 +0930

    setup.py updated
    added required dependencys

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
-
+import setuptools
 setup(name='mylib',
       version='0.1',
       description='A collection of useful functions',
       author='Robin Keegan-Treloar',
       author_email='robin_kt@outlook.com',
+      url='https://github.com/robinkeegan/mylib',
       license='MIT',
-      packages=['mylib'],
+      packages=setuptools.find_packages(),
       zip_safe=False)
 
